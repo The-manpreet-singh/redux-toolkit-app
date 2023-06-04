5120e3a295763e3476de025c76efe8edae0ce9a6
Manpreet Singh <manpreetsingh@Manpreets-Air.domain.name>
2023-06-04 16:43:26 +0530

    add extra reducer with create action

--- src/components/DeleteAllUser.jsx
+++ src/components/DeleteAllUser.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import styled from "styled-components";
-import { deleteUsers } from "../store/slices/userSlice";
+//import { deleteUsers } from "../store/slices/userSlice";
 import { useDispatch } from "react-redux";
+import { deleteAllUsers } from "../store/actions";
 
 export const DeleteAllUser = () => {
   const dispatch = useDispatch();
 
-  const deleteAllUsers = () => {
-    dispatch(deleteUsers());
+  const deleteAllUser = () => {
+    dispatch(deleteAllUsers());
   };
   return (
     <Wrapper>
-      <button className="btn clear-btn" onClick={deleteAllUsers}>
+      <button className="btn clear-btn" onClick={deleteAllUser}>
         clear users
       </button>
     </Wrapper>
--- src/store/slices/UserSlice.jsx
+++ src/store/slices/UserSlice.jsx
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { deleteAllUsers } from "../actions";
 
 const userSlice = createSlice({
   name: "user",
@@ -12,13 +13,18 @@ const userSlice = createSlice({
       //console.log("delete:", action.payload);
       state.splice(action.payload, 1);
     },
-    deleteUsers(state, action) {
-      //console.log("delete all users:");
+    // deleteUsers(state, action) {
+    //   //console.log("delete all users:");
+    //   return [];
+    // },
+  },
+  extraReducers(buider) {
+    buider.addCase(deleteAllUsers, () => {
       return [];
-    },
+    });
   },
 });
 
 export { userSlice };
 //console.log(userSlice.actions); //this is action creators so we need to export it
-export const { addUsers, removeUser, deleteUsers } = userSlice.actions;
+export const { addUsers, removeUser } = userSlice.actions;
